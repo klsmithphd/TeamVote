b50121158de5d3b39842b98767c836f5cafec8b2
Ken Smith <ken.smith@2scientists.net>
2014-11-11 18:19:59 -0500

    updated teams listing

--- app/client/templates/teams/teams_list.js
+++ app/client/templates/teams/teams_list.js
@@ -1,15 +1,38 @@
 var teamsData = [
     {
         teamId : 1,
-        teamName : 'Lunch'
+        teamName : 'Exhibit' 
+        teamMembers : 'Nathan, James, Jon'
     },
     {
         teamId : 2,
-        teamName : 'Foo'
+        teamName : 'Catnip'
+        teamMembers: 'Pete, Chris G, Craig, Jason, Derek, Yoshi, David L, Kris'
     },
     {
         teamId : 3,
-        teamName : 'Bar'
+        teamName : 'Project Timelines'
+        teamMembers: 'Ken I, Kevin Chu'
+    },
+    {
+        teamId: 4,
+        teamName: 'MapMash'
+        teamMembers: 'Andrew M'
+    },
+    {
+        teamId: 5,
+        teamName: 'UI Test Generator'
+        teamMembers: 'David T'
+    },
+    {
+        teamId: 6,
+        teamName: 'Quick Cliplet'
+        teamMembers: 'Travis'
+    },
+    {
+        teamId: 7,
+        teamName: 'Mission Control'
+        teamMembers: 'Chris T'
     }
 ];
 
