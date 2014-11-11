871dde304b3dea1329045e48450f907d7a7029b4
Ken Smith <ken.smith@2scientists.net>
2014-11-11 18:30:11 -0500

    Prep for vote saving

--- app/client/templates/ballot/ballot.js
+++ app/client/templates/ballot/ballot.js
@@ -1,9 +1,13 @@
 Template.ballot.events({
     'submit form': function(e) {
         e.preventDefault();
-        console.log( $(e.target).find('[name=options1]:checked').val() );
-        console.log( $(e.target).find('[name=options2]:checked').val() );
-        console.log( $(e.target).find('[name=options3]:checked').val() );
-        console.log( e );
+
+        var vote = {
+            award1: $(e.target).find('[name=options1]:checked').val(),
+            award2: $(e.target).find('[name=options2]:checked').val(),
+            award3: $(e.target).find('[name=options3]:checked').val()
+        };
+
+        vote._id = Votes.insert(vote);
     }
 });
